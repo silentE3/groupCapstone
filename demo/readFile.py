4f624721999ad05c7e9e32581f3bc6d6e4867e0e
silentE3 <edwardchan2@comcast.net>
2022-10-13 18:59:10 -0700

    Working in progress: figuring out how to use the read methods for a given file

--- demo/readFile.py
+++ demo/readFile.py
@@ -1,9 +1,18 @@
 file1 = open("readTest.txt", "r");
 
+#This part will consist of expressions that get the number of lines in the text file.
+count = len(file1.readlines())
+print("Number of lines: " + str(count))
+
 #This part consists of getting every line in the text file one by one using a for loop.
 for line in file1:
         n_line = line.strip();
         print(n_line);
+file1.close();
+
+file2 = open("readTest.txt", "r");
+
+#This part consists of printing out every single line at once as an array.
+print(file2.readlines())
 
 
-#This part will consist of expressions that get the number of lines in the text file.
