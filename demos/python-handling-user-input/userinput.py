8d2c6f80af1c33e1882f0de00c4cb006ad197c04
silentE3 <edwardchan2@comcast.net>
2022-10-17 00:32:34 -0700

    Made some adjustments to the code to make the input an int for comparison statements

--- demos/python-handling-user-input/userinput.py
+++ demos/python-handling-user-input/userinput.py
@@ -1,13 +1,13 @@
 #This demo is in the form of a grading program that given a grade based on the value given.
 grade_percent = input("Enter in your grade percent: ")
 
-if (grade_percent >= 90):
+if (int(grade_percent) >= 90):
     print("You got an A!!")
-elif(grade_percent < 90 and grade_percent >= 80):
+elif(int(grade_percent) < 90 and int(grade_percent) >= 80):
     print("You got a B!")
-elif(grade_percent < 80 and grade_percent >= 70):
+elif(int(grade_percent) < 80 and int(grade_percent) >= 70):
     print("You got a C.")
-elif(grade_percent < 70 and grade_percent >= 60):
+elif(int(grade_percent) < 70 and int(grade_percent) >= 60):
     print("You got a D.")
 else:
     print("You got an E")
