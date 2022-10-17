4d1145d13fd18f4b801ff16d49e66a129d3b7c09
silentE3 <edwardchan2@comcast.net>
2022-10-17 00:27:45 -0700

    Created a grading program for demo

--- demos/python-handling-user-input/userinput.py
+++ demos/python-handling-user-input/userinput.py
@@ -1,2 +1,13 @@
 #This demo is in the form of a grading program that given a grade based on the value given.
 grade_percent = input("Enter in your grade percent: ")
+
+if (grade_percent >= 90):
+    print("You got an A!!")
+elif(grade_percent < 90 and grade_percent >= 80):
+    print("You got a B!")
+elif(grade_percent < 80 and grade_percent >= 70):
+    print("You got a C.")
+elif(grade_percent < 70 and grade_percent >= 60):
+    print("You got a D.")
+else:
+    print("You got an E")
