8df29323938f9b06a8c0aca939af8b9d0f81fbaf
BrandenBoucher <bgbouche@asu.edu>
2022-10-21 19:52:39 -0700

    update cli for grouping and stubbed out some files

--- app/commands/cli.py
+++ app/commands/cli.py
@@ -1,13 +1,16 @@
 '''
 cli contains the main group for commands
 '''
+import imp
 import click
 from app.commands import gen
+from app.commands import grouping
 
 @click.group()
 def cli():
     '''
-    cli is the main group for all commands
+    
     '''
 
+cli.add_command(grouping.group)
 cli.add_command(gen.gen)
--- app/commands/gen.py
+++ app/commands/gen.py
@@ -6,15 +6,9 @@ from app import generate
 from app.file import output
 
 @click.group("gen")
-def gen():
-    '''
-    commands for generating datasets
-    '''
-
-@gen.command("dataset")
 @click.option('--filename', default="dataset.csv")
 @click.option('--count', default=20)
-def dataset(filename: str, count: int):
+def gen(filename: str, count: int):
     '''
     dataset generates a new sample dataset containing
     the necessary criteria to use for running the algorithm
@@ -28,3 +22,6 @@ def dataset(filename: str, count: int):
         'disliked 1', 'disliked 2', 'disliked 3'
     ]
     output.output_to_csv(headers, body, filename)
+
+
+
