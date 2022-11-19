a307c748a4c09d8e15e4222cc76a6127f5cb8aa6
silentE3 <edwardchan2@comcast.net>
2022-11-18 17:06:38 -0800

    WIP Figuring out how to program the sorting part of my prototype part 2.

--- app/grouping/sorting.py
+++ app/grouping/sorting.py
@@ -1,6 +1,7 @@
 '''
 This file contains a class that finds unique groups.
 '''
+import itertools
 from app import models
 
 class UniqueGrouper:
@@ -11,4 +12,24 @@ class UniqueGrouper:
         '''
         This method finds all possible group combinations in the survey and removes any duplicates.
         '''
-        return []
+        # Determine if non-target groups are larger or smaller (or if N/A)
+        # (0 = all standard, 1 = some larger, -1 = some smaller)
+        non_stand_mod = 0
+        if (num_groups * target_group_size) < len(survey_data):
+            non_stand_mod = 1
+        elif (num_groups * target_group_size) > len(survey_data):
+            non_stand_mod = -1
+
+        # Determine number of groups with non-target size
+        num_non_targ_groups = 0
+        if non_stand_mod == 1:
+            num_non_targ_groups = len(survey_data) % target_group_size
+        if non_stand_mod == -1:
+            num_non_targ_groups = target_group_size - (len(survey_data) % target_group_size)
+
+        result: list[models.GroupRecord]
+        result = []
+
+        #This part focuses on finding all possible group combinations of the target size
+        print(itertools.combinations(survey_data, target_group_size))
+        return result
