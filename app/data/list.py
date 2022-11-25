31445653db156510d1dac6a0ac2573e0164af9a5
silentE3 <edwardchan2@comcast.net>
2022-11-25 14:24:19 -0800

    Added more progress to function and changed the function to add asurite ids instead of names

--- app/data/list.py
+++ app/data/list.py
@@ -7,15 +7,16 @@ class StudentList:
     '''
     This class provides a reader to the survey data and student list.
     '''
-    def addStudents(self, survey: list[models.SurveyRecord], roster: list[str]):
+    def add_students(self, survey: list[models.SurveyRecord], roster: list[str]) -> list[models.SurveyRecord]:
         '''
         This method involves reading the survey data and adding any missing students from the student list
-        to the survey data.
+        to the survey data. This will be based on student id.
         '''
+        new_survey_data = survey
         survey_students = []
         for entry in survey:
-            survey_students.append(entry.student_name)
-        
+            survey_students.append(entry.student_id)
+
         #At this point, the survey_student list should have every student name from the survey data.
         #Next, we will check if all the students have answered the survey.
 
@@ -23,3 +24,13 @@ class StudentList:
         for student in roster:
             if survey_students.count(student) == 0:
                 missing_students.append(student)
+
+        #At this point, all missing students should be in the missing students list.
+        for asurite in missing_students:
+            record = models.SurveyRecord (
+                student_id=asurite
+            )
+            #This code will use the function that adds availiability to all time slots.
+            new_survey_data.append(record)
+
+        return new_survey_data
