b396d2c00991b4537286d357bd4cc45b8a000cc0
mmcgehe2 <92449086+mmcgehe2@users.noreply.github.com>
2022-11-25 09:26:39 -0600

    Task #110: Implement scoring function per updated design (#41)
        * Consider "additional overlapping time slots" as a 4th priority
        * Use more intuitive scoring:
            - Higher score = better solution
            - Non-negative score means that both "hard" constraints were met
            - Individual group scores sum to the total solution score

--- app/group/scoring.py
+++ app/group/scoring.py
@@ -9,45 +9,59 @@ from app.group import validate
 
 def score_groups(variables: models.GroupSetData) -> float:
     '''
-    This function scores a group set (entire grouping solution) based on:
+    Scores a group set (entire grouping solution) and returns the score value as a float,
+        where a HIGHER score indicates a better solution.
+
+    The grouping solution is scored based on (in priority order):
         - number of disliked pairings
         - number of groups without an overlapping time slot
         - number of preferred/liked pairings
+        - number of "additional" overlapping timeslots
+            NOTE: "Additional" meaning beyond the one overlapping slot "required" per group.
 
     Additionally, the scores are computed relative to a few fixed values within the solution
      set. These are:
         - number of students
         - max number of groups possible
         - number of preferred student slots on the student survey
+        - number of time slots on the student survey
 
     The scoring equation used is as follows:
-        T (s, d, p ) = C1 + C2 * s + C3 * d – 0.1 * p
-                s = number of groups without an overlapping time slot.
-                d = number of disliked pairings.
+        S (p, t, s, d ) = (0.1 * p) + (C4 * t) – (C2 * s) – (C3 * d)
                 p = number of preferred/liked pairings
+                t = number of additional overlapping time slots
+                s = number of groups without an overlapping time slot
+                d = number of disliked pairings
 
-            C1, C2, C3 are considered constants for each solution set:
+            C1, C2, C3, and C4 are considered constants for each solution set:
                 C1 = (0.1 * N * P)
                 C2 = C1 + 1
                 C3 = C2 * G + C1 + 1
+                C4 = 0.1/(G * T)
 
                 Where,
                     N = the total number of students being grouped
-                    P = number of preferred/liked student slots on the survey
+                    P = number of preferred/liked student slots on the student survey
                     G = max number of groups possible (based on the target group size +/- 1)
+                    T = number of time slots on the student survey
 
         NOTE: This equation guarantees the following about a solution (groupings), relative to
          other solutions within the solution set:
-            - The solution with greater disliked pairings will always have the worse (higher) score
+            - A solution with greater disliked pairings will always have the worse (lower) score.
             - A solution with the same number of disliked pairings as another, but fewer groups
-                with an overlapping time slot, will always have a worse (higher) score.
+                with an overlapping time slot, will always have a worse (lower) score.
             - A solution with the same number of disliked pairings AND groups with an overlapping
-                time slot as another, but more preferred pairings will always score better (lower).
+                time slot as another, but more preferred pairings will always score better (higher)
+            - A solution with the same number of disliked pairings AND groups with an overlapping
+                time slot AND preferred pairings as another, but more “additional overlapping time
+                slots” will always score better (higher).
+
 
     '''
     total_score: float = 0
-    constant_1: float = 0.1 * variables.num_of_students * \
-        variables.num_of_preferred_slots
+
+    constant_1: float = 0.1 * variables.num_students * \
+        variables.num_survey_preferred_slots
     constant_2: float = constant_1 + 1
 
     max_num_groups_pos: int  # based on target group size
@@ -55,28 +69,34 @@ def score_groups(variables: models.GroupSetData) -> float:
         # divide by 0 and invalid target group size protection
         variables.target_group_size = 1
         max_num_groups_pos = trunc(
-            variables.num_of_students / variables.target_group_size)
+            variables.num_students / variables.target_group_size)
     else:
         max_num_groups_pos = trunc(
-            variables.num_of_students / (variables.target_group_size - 1))
+            variables.num_students / (variables.target_group_size - 1))
     constant_3: float = constant_2 * max_num_groups_pos + constant_1 + 1
+    constant_4: float = 0.1/(max_num_groups_pos *
+                             variables.num_survey_time_slots)
 
-    total_score = constant_1 + (constant_2 * variables.num_groups_no_overlap) + (
-        constant_3 * variables.num_of_disliked_pairs) - (0.1 * variables.num_of_preferred_pairs)
-    return round(total_score, 2)
+    total_score = ((0.1 * variables.num_preferred_pairs) +
+                   (constant_4 * variables.num_additional_overlap) -
+                   (constant_2 * variables.num_groups_no_overlap) -
+                   (constant_3 * variables.num_disliked_pairs))
+    return round(total_score, 4)
 
 
 def score_individual_group(group: models.GroupRecord, variables: models.GroupSetData) -> float:
     '''
-    This function scores an individual group using the T (s, d, p ) equation in the score_groups
-     function, but with s, d, and p being specific to the group (i.e., s is 0 or 1 for the group,
+    This function scores an individual group using the S (p, t, s, d) equation in the score_groups
+     function, but with p, t, s, and d being specific to the group (i.e., s is 0 or 1 for the group,
      d is number of disliked pairings within the group, etc.).
 
     '''
-    variables.num_of_disliked_pairs = validate.total_disliked_pairings([group])
-    variables.num_of_preferred_pairs = validate.total_liked_pairings([group])
+    variables.num_disliked_pairs = validate.total_disliked_pairings([group])
+    variables.num_preferred_pairs = validate.total_liked_pairings([group])
     variables.num_groups_no_overlap = \
         validate.total_groups_no_availability([group])
+    variables.num_additional_overlap = max(validate.availability_overlap_count(
+        group) - 1, 0)  # subtract one to get "additional"
     return score_groups(variables)
 
 
