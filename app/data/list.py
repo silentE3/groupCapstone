10d00bd2e1b4df91151bc1aaff67e04a2151072b
silentE3 <edwardchan2@comcast.net>
2022-11-29 03:55:40 -0800

    Fix ident and changed method name

--- app/data/list.py
+++ app/data/list.py
@@ -4,7 +4,7 @@ This file will be in charge of adding missing students to the list.
 from app import models
 from app.data import load
 
-def add_students(survey: list[models.SurveyRecord], roster: list[str]) -> list[models.SurveyRecord]:
+def add_missing_students(survey: list[models.SurveyRecord], roster: list[str]) -> list[models.SurveyRecord]:
     '''
     This method involves reading the survey data and adding any missing students from the student list
     to the survey data. This will be based on student id.
@@ -22,13 +22,13 @@ def add_students(survey: list[models.SurveyRecord], roster: list[str]) -> list[m
         if survey_students.count(student) == 0:
             missing_students.append(student)
 
-        #At this point, all missing students should be in the missing students list.
-        for asurite in missing_students:
-            record = models.SurveyRecord (
-                student_id=asurite
-            )
-            #This code will use the function that adds availiability to all time slots.
-            record.availability = load.set_wildcard_availability(record)
-            new_survey_data.append(record)
+    #At this point, all missing students should be in the missing students list.
+    for asurite in missing_students:
+        record = models.SurveyRecord (
+            student_id=asurite
+        )
+        #This code will use the function that adds availiability to all time slots.
+        record.availability = load.set_wildcard_availability(record)
+        new_survey_data.append(record)
 
     return new_survey_data
