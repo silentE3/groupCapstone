879b8016daea0c1ca8092417ec44ef634cbd9933
silentE3 <edwardchan2@comcast.net>
2022-11-28 20:44:24 -0800

    Changed the list function for adding missing students to just a method

--- app/data/list.py
+++ app/data/list.py
@@ -4,27 +4,23 @@ This file will be in charge of adding missing students to the list.
 from app import models
 from app.data import load
 
-class StudentList:
+def add_students(survey: list[models.SurveyRecord], roster: list[str]) -> list[models.SurveyRecord]:
     '''
-    This class provides a reader to the survey data and student list.
+    This method involves reading the survey data and adding any missing students from the student list
+    to the survey data. This will be based on student id.
     '''
-    def add_students(self, survey: list[models.SurveyRecord], roster: list[str]) -> list[models.SurveyRecord]:
-        '''
-        This method involves reading the survey data and adding any missing students from the student list
-        to the survey data. This will be based on student id.
-        '''
-        new_survey_data = survey
-        survey_students = []
-        for entry in survey:
-            survey_students.append(entry.student_id)
+    new_survey_data = survey
+    survey_students = []
+    for entry in survey:
+        survey_students.append(entry.student_id)
 
-        #At this point, the survey_student list should have every student name from the survey data.
-        #Next, we will check if all the students have answered the survey.
+    #At this point, the survey_student list should have every student name from the survey data.
+    #Next, we will check if all the students have answered the survey.
 
-        missing_students = []
-        for student in roster:
-            if survey_students.count(student) == 0:
-                missing_students.append(student)
+    missing_students = []
+    for student in roster:
+        if survey_students.count(student) == 0:
+            missing_students.append(student)
 
         #At this point, all missing students should be in the missing students list.
         for asurite in missing_students:
@@ -35,4 +31,4 @@ class StudentList:
             record.availability = load.set_wildcard_availability(record)
             new_survey_data.append(record)
 
-        return new_survey_data
+    return new_survey_data
