3059a6759adfce76ab3ed589240e4c9b66eaf9e8
zredinger-ccc <zach@cloud-team.com>
2023-01-16 20:05:28 -0600

    update build.py

--- build-scripts/build.py
+++ build-scripts/build.py
@@ -4,13 +4,13 @@ Build script for compilation via PyInstaller
 The following options are used:
 
 `--onefile` - places the program into a single executable
-`--specfile grouper.spec` - static spec file that ensures the same config every time it is compiled
+`--specpath grouper.spec` - static spec file that ensures the same config every time it is compiled
 """
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
     'grouper.py',
     '--onefile',
-    '--specfile',
+    '--specpath',
     'grouper.spec'
 ])
