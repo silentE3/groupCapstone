28eab619335341ab2e78f198ba0c25a4898b06a6
Mackenzie McGehee <mmcgehe2@asu.edu>
2023-01-21 08:53:12 -0600

    Update default grouping behavior to include creation of the report

--- app/commands/group.py
+++ app/commands/group.py
@@ -15,7 +15,7 @@ from app.grouping import grouper_2
 @click.argument('surveyfile', type=click.Path(exists=True), default='dataset.csv')
 @click.option('-o', '--outputfile', show_default=False, default=None, help="Enter the path to the output file. [default: <SURVEYFILE>_groups]")
 @click.option('-c', '--configfile', show_default=True, default="config.json", help="Enter the path to the config file.", type=click.Path(exists=True))
-@click.option('--report/--no-report', show_default=True, default=False, help="Use this option to output a report on the results of the goruping.")
+@click.option('--report/--no-report', show_default=True, default=True, help="Use this option to output a report on the results of the goruping.")
 @click.option('-r', '--reportfile', show_default=False, default=None,
               help="report filename, relies on --report flag being enabled [default: <outputfile>_report.csv]")
 @click.option('-a', '--allstudentsfile', help="list of all student ids in class. Ignored if not included")
