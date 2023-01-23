f2692bc5451e20ba48dacf6c14a0acb78aa289a8
Mackenzie McGehee <mmcgehe2@asu.edu>
2023-01-23 11:31:37 -0600

    Prevent overwriting of existing output files by appending an additional number when necessary

--- app/commands/group.py
+++ app/commands/group.py
@@ -3,6 +3,7 @@ group contains all the commands for reading in the .csv data files and generatin
 Also includes reading in the configuration file.
 '''
 
+from pathlib import Path
 import click
 from app import config, core, models, output
 from app.data import load, reporter
@@ -26,25 +27,29 @@ def group(surveyfile: str, outputfile: str, configfile: str, report: bool, repor
     SURVEYFILE is path to the raw survey output. [default=dataset.csv]
     '''
 
-    # Set the default output filename values per the input filename (SURVEYFILE) value (if
-    #  output file values were not specified)
-    if outputfile is None:
-        outputfile = f'{surveyfile.removesuffix(".csv")}_groups'
-    if report and reportfile is None:
-        # the default report filename is based upon the output filename
-        reportfile = f'{outputfile.removesuffix(".csv")}_report'
+    ########## Determine Output Filenames ##########
+    filenames: list[str] = __determine_output_filenames(
+        surveyfile, outputfile, report, reportfile)
+    output_filename_1: str = filenames[0]
+    output_filename_2: str = filenames[1]
+    report_filename: str = filenames[2]
 
+    ########## Load the config data ##########
     config_data: models.Configuration = config.read_json(configfile)
 
+    ########## Load the survey data ##########
     records: list[models.SurveyRecord] = load.read_survey(
         config_data['field_mappings'], surveyfile)
 
+    ########## Load the class roster data, if applicable ##########
     if allstudentsfile:
         click.echo(
             f'checking roster for missing students in {allstudentsfile}')
         roster = load.read_roster(allstudentsfile)
         records = load.add_missing_students(
             records, roster, config_data['field_mappings']['availability_field_names'])
+
+    ########## Grouping ##########
 
     # Perform pre-grouping error checking
     if core.pre_group_error_checking(config_data["target_group_size"], records):
@@ -64,11 +69,9 @@ def group(surveyfile: str, outputfile: str, configfile: str, report: bool, repor
         records, config_data, min_max_num_groups[0], min_max_num_groups[1])
 
     # Output results
-    # ensure the output filename ends .csv
-    outputfile_1: str = f'{outputfile.removesuffix(".csv")}_1.csv'
-    click.echo(f'writing groups to {outputfile_1}')
+    click.echo(f'writing groups to {output_filename_1}')
     output.GroupingDataWriter(config_data).write_csv(
-        best_solution_grouper_1.best_solution_found, outputfile_1)
+        best_solution_grouper_1.best_solution_found, output_filename_1)
 
     ########## Run "second" grouping algorithm ##########
 
@@ -77,19 +80,15 @@ def group(surveyfile: str, outputfile: str, configfile: str, report: bool, repor
         records, config_data, min_max_num_groups[0], min_max_num_groups[1])
 
     # Output results
-    # ensure the output filename ends .csv
-    outputfile_2: str = f'{outputfile.removesuffix(".csv")}_2.csv'
-    click.echo(f'writing groups to {outputfile_2}')
+    click.echo(f'writing groups to {output_filename_2}')
     output.GroupingDataWriter(config_data).write_csv(
-        best_solution_grouper_2, outputfile_2)
+        best_solution_grouper_2, output_filename_2)
 
     ########## Output solutions report if configured ##########
     if report:
         solutions: list[list[models.GroupRecord]] = [
             best_solution_grouper_1.best_solution_found, best_solution_grouper_2]
-        # ensure the report filename ends .xlsx
-        report_filename = f'{reportfile.removesuffix(".xlsx")}.xlsx'
-        click.echo(f'Writing report to: "{report_filename}"')
+        click.echo(f'Writing report to: {report_filename}')
         reporter.write_report(
             solutions, config_data, report_filename)
 
@@ -124,3 +123,44 @@ def __run_grouping_alg_2(records: list[models.SurveyRecord], config_data: models
             score = grouper2.grade_groups()
             best_solution_found = group_result
     return best_solution_found
+
+
+def __determine_output_filenames(surveyfile: str, outputfile: str, report: bool, reportfile: str) -> list[str]:
+    filenames: list[str] = []
+
+    # Set the default output filename values per the input filename (SURVEYFILE) value (if
+    #  output file values were not specified)
+    if outputfile is None:
+        outputfile = f'{surveyfile.removesuffix(".csv")}_groups'
+    if report and reportfile is None:
+        # the default report filename is based upon the output filename
+        reportfile = f'{outputfile.removesuffix(".csv")}_report'
+
+    # ensure the output filenames end .csv
+    output_filename_1: str = f'{outputfile.removesuffix(".csv")}_1.csv'
+    output_filename_2: str = f'{outputfile.removesuffix(".csv")}_2.csv'
+
+    # ensure the report filename ends .xlsx
+    report_filename: str = ""
+    if report:
+        report_filename = f'{reportfile.removesuffix(".xlsx")}.xlsx'
+
+    # Append integer value to avoid output file overwriting
+    for filename in [output_filename_1, output_filename_2, report_filename]:
+        suffix: str = '.xlsx' if filename == report_filename else '.csv'
+        if Path('./' + filename).is_file():
+            print(filename + " already exists...", end='')
+            append_value: int = 1
+            new_filename: str = filename.removesuffix(
+                suffix) + '_' + str(append_value) + suffix
+            while (Path('./' + new_filename).is_file()):
+                append_value += 1
+                new_filename = filename.removesuffix(
+                    suffix) + '_' + str(append_value) + suffix
+            print('available filename: ' + new_filename)
+            filenames.append(new_filename)
+        else:
+            filenames.append(filename)
+    print()
+
+    return filenames
