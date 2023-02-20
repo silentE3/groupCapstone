0f799b20530958d7699708a22348b7ee620f1458
silentE3 <edwardchan2@comcast.net>
2023-02-19 18:48:52 -0800

    WIP Adding tests to test config file

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,6 +4,7 @@ Tests reading in a configuration file
 # Note: Only verification of the group size parameter is specifically covered here at this time.
 
 from app import config
+from app.models import Configuration
 
 
 def test_config_target_group_size_2():
@@ -44,3 +45,14 @@ def test_config_target_group_size_negative():
     configuration = config.read_json(
         "./tests/test_files/configs/config_negative_group_size.json")
     assert configuration["target_group_size"] == expected_target_group_size
+
+def test_read_report_config():
+    '''
+    Tests if the config file properly reads the config tab in the report.
+    '''
+    result:Configuration = config.read_report_config("Example_Report_1.xlsx")
+
+    assert result.get("class_name") == "SER401"
+    assert result.get("target_group_size") == 2
+    assert result.get("target_plus_one_allowed") == True
+    assert result.get("target_minus_one_allowed") == False
