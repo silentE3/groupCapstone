b654d1ae9b32132b0c0e08fe12efc41b0410bb58
Armand <aaaltive@asu.edu>
2023-02-24 21:09:16 -0700

    remove report option from program since creating the report is the default action

--- app/commands/report.py
+++ app/commands/report.py
@@ -8,34 +8,6 @@ from app import config, models
 from app.data import load, reporter
 
 
-@click.command("report")
-@click.argument('groupfile', type=click.Path(exists=True), default="output.csv")
-@click.argument('surveyfile', type=click.Path(exists=True), default="dataset.csv")
-@click.option('-c', '--configfile', type=click.Path(exists=True), show_default=True, default="config.json", help="Enter the path to the config file.")
-@click.option('-r', '--reportfile', show_default=True, default="grouping_results_report.xlsx",
-              help="Enter the path to the group report output file.")
-def report(groupfile: str, surveyfile: str, reportfile: str, configfile: str):
-    '''Generate report- Creates a report on the results of the groups that were generated. 
-    It uses the raw survey file to verify the data.
-
-    GROUPFILE is the path to the grouped dataset. [default=output.csv]
-
-    SURVEYFILE is path to the raw survey output. [default=dataset.csv]
-    '''
-
-    # load config data and survey data reader
-    config_data: models.Configuration = config.read_json(configfile)
-
-    # load the survey data
-    survey_data = load.read_survey(config_data['field_mappings'], surveyfile)
-
-    groups = load.read_groups(groupfile, survey_data.records)
-
-    click.echo(f'Writing report to: "{reportfile}"')
-    reporter.write_report(
-        [groups], survey_data.raw_rows, config_data, reportfile)
-
-
 @click.command("update-report")
 @click.argument('reportfile', type=click.Path(exists=True), default="group_report.xlsx")
 def update_report(reportfile: str):
