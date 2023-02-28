31bfd6af46be3fec12d69588ec155af4d36520bf
Armand <aaaltive@asu.edu>
2023-02-27 23:42:01 -0700

    begin playing with a guided cli experience

--- app/commands/cli.py
+++ app/commands/cli.py
@@ -2,18 +2,37 @@
 cli contains the main group for commands
 '''
 
+
 import click
 from app.commands import generate, group, report
+from importlib import import_module
 
 
-@click.group()
-def cli():
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
     '''
     commands for the grouping tool
     '''
+    # If no subcommand is provided, invoke the guide command
+    if ctx.invoked_subcommand is None:
+        ctx.invoke(guide)
 
+@cli.command()
+def guide():
+    '''
+    guide command
+    '''
+    command_names = ['group', 'generate', 'report', 'update_report']
+    click.echo('Available commands:')
+    for name in command_names:
+        click.echo(f'* {name}')
+    selected_command = click.prompt('Select a command to run', type=click.Choice(command_names))
+    # Import the selected command module
+    module = import_module(f'app.commands.{selected_command}')
+    # Invoke the selected command
+    ctx = click.get_current_context()
+    ctx.invoke(getattr(module, selected_command))
 
-cli.add_command(group.group)
-cli.add_command(generate.gen)
-cli.add_command(report.report)
-cli.add_command(report.update_report)
+if __name__ == '__main__':
+    cli()
