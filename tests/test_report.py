9e3bd4b339ea5bca46dd6c79c8acf80ec6d006a8
Armand <aaaltive@asu.edu>
2023-02-28 23:37:38 -0700

    get rid of tests that test the report command since it no longer exists

--- tests/test_report.py
+++ tests/test_report.py
@@ -11,74 +11,6 @@ runner = CliRunner()
 # These tests verify the functionality in report.py.
 
 
-def test_dev_reporting_basic():
-    '''
-    Runs a simple test against the dev data set 
-    '''
-    response = runner.invoke(report.report, [
-                             './tests/test_files/dev_data/output.csv',
-                             './tests/test_files/dev_data/dataset-dev.csv',
-                             '-c', './tests/test_files/dev_data/config-dev.json'])
-    assert response.exit_code == 0
-
-    assert exists("./grouping_results_report.xlsx")
-
-    os.remove("./grouping_results_report.xlsx")
-
-
-def test_dev_reporting_report_file_specified():
-    '''
-    Runs a simple test against the dev data set but
-    specifies the report output file name
-    '''
-    response = runner.invoke(report.report, [
-                             './tests/test_files/dev_data/output.csv',
-                             './tests/test_files/dev_data/dataset-dev.csv',
-                             '-c', './tests/test_files/dev_data/config-dev.json',
-                             '-r', './tests/test_files/dev_data/specific_report.xlsx'])
-    assert response.exit_code == 0
-
-    assert exists("./tests/test_files/dev_data/specific_report.xlsx")
-
-    os.remove("./tests/test_files/dev_data/specific_report.xlsx")
-
-
-def test_invalid_group_file():
-    '''
-    Runs a simple test to ensure it fails on a bad file
-    '''
-    response = runner.invoke(report.report, [
-                             './tests/test_files/dev_data/badfile.csv',
-                             './tests/test_files/dev_data/dataset-dev.csv',
-                             '-c', './tests/test_files/dev_data/config-dev.json',
-                             '-r', './tests/test_files/dev_data/specific_report.xlsx'])
-    assert response.exit_code == 2
-
-
-def test_invalid_survey_file():
-    '''
-    Runs a simple test to ensure it fails on a bad file
-    '''
-    response = runner.invoke(report.report, [
-                             './tests/test_files/dev_data/output.csv',
-                             './tests/test_files/dev_data/badfile.csv',
-                             '-c', './tests/test_files/dev_data/config-dev.json',
-                             '-r', './tests/test_files/dev_data/specific_report.xlsx'])
-    assert response.exit_code == 2
-
-
-def test_invalid_config_file():
-    '''
-    Runs a simple test to ensure it fails on a bad file
-    '''
-    response = runner.invoke(report.report, [
-                             './tests/test_files/dev_data/output.csv',
-                             './tests/test_files/dev_data/dataset-dev.csv',
-                             '-c', './tests/test_files/dev_data/badfile.json',
-                             '-r', './tests/test_files/dev_data/specific_report.xlsx'])
-    assert response.exit_code == 2
-
-
 def test_update_report_basic():
     '''
     Runs a simple "update report" test against Example_Report_1.xlsx.
@@ -108,22 +40,3 @@ def test_update_report_invalid_report_file():
     response = runner.invoke(report.update_report, [
                              './tests/test_files/reports/Nonexistent_File.xlsx'])
     assert response.exit_code == 2
-
-def test_dev_reporting_contains_sheets():
-    '''
-    Checks the generated excel file for the expected sheets
-    For now, just config
-    '''
-    response = runner.invoke(report.report, [
-                             './tests/test_files/dev_data/output.csv',
-                             './tests/test_files/dev_data/dataset-dev.csv',
-                             '-c', './tests/test_files/dev_data/config-dev.json'])
-    assert response.exit_code == 0
-
-    assert exists("./grouping_results_report.xlsx")
-
-    book: workbook.Workbook = load_workbook("./grouping_results_report.xlsx")
-    assert 'config' in list(book.sheetnames)
-    # add more asserts for more sheet names here
-
-    os.remove("./grouping_results_report.xlsx")
