6a7d61b3137fb4b7c26b6b38aa2570a01d163b86
Zach Redinger <63171254+zredinger@users.noreply.github.com>
2023-03-01 14:18:29 -0600

    Tsk#234: Ability to format cells (#94)
    * add ability to apply formats to cells
    
    * update documentation and specify new green color
    
    * get tests working

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,5 +1,6 @@
 from app.data import reporter
 from app import config, models
+from app.file import xlsx
 
 
 def test_format_group_report_check_header():
@@ -16,7 +17,7 @@ def test_format_group_report_check_header():
     data_config: models.Configuration = config.read_json(
         "./tests/test_files/configs/config_1.json")
     data_config["report_fields"] = report_config
-    report_formatter = reporter.ReportFormatter(data_config)
+    report_formatter = reporter.ReportFormatter(data_config, {})
 
     groups = [models.GroupRecord("1", [
         models.SurveyRecord('asurite1', preferred_students=[
@@ -29,8 +30,8 @@ def test_format_group_report_check_header():
 
     report = report_formatter.format_group_report(groups)
 
-    assert report[0] == ['Group Id', 'Group Size', 'Meets Dislike Requirement', 'Meets Availability Requirement',
-                         'Availability Overlap', 'Meets Preferred Goal', 'Preferred pairs in group', 'Preferred pair count']
+    assert report[0] == [xlsx.Cell('Group Id'), xlsx.Cell('Group Size'), xlsx.Cell('Meets Dislike Requirement'), xlsx.Cell('Meets Availability Requirement'),
+                         xlsx.Cell('Availability Overlap'), xlsx.Cell('Meets Preferred Goal'), xlsx.Cell('Preferred pairs in group'), xlsx.Cell('Preferred pair count')]
 
 
 def test_format_group_report_check_header_show_disliked():
@@ -47,7 +48,7 @@ def test_format_group_report_check_header_show_disliked():
     data_config: models.Configuration = config.read_json(
         "./tests/test_files/configs/config_1.json")
     data_config["report_fields"] = report_config
-    report_formatter = reporter.ReportFormatter(data_config)
+    report_formatter = reporter.ReportFormatter(data_config, {})
 
     groups = [models.GroupRecord("1",
                                  [
@@ -64,8 +65,8 @@ def test_format_group_report_check_header_show_disliked():
 
     report = report_formatter.format_group_report(groups)
 
-    assert report[0] == ['Group Id', 'Group Size', 'Meets Dislike Requirement', 'Disliked students in group',
-                         'Meets Availability Requirement', 'Meets Preferred Goal', 'Disliked pairs in group', 'Disliked pair count', 'Score']
+    assert report[0] == [xlsx.Cell('Group Id'), xlsx.Cell('Group Size'), xlsx.Cell('Meets Dislike Requirement'), xlsx.Cell('Disliked students in group'),
+                         xlsx.Cell('Meets Availability Requirement'), xlsx.Cell('Meets Preferred Goal'), xlsx.Cell('Disliked pairs in group'), xlsx.Cell('Disliked pair count'), xlsx.Cell('Score')]
 
 
 def test_format_group_report_check_group():
@@ -79,7 +80,7 @@ def test_format_group_report_check_group():
     data_config: models.Configuration = config.read_json(
         "./tests/test_files/configs/config_1.json")
     data_config["report_fields"] = report_config
-    report_formatter = reporter.ReportFormatter(data_config)
+    report_formatter = reporter.ReportFormatter(data_config, {})
 
     groups = [models.GroupRecord("1",
                                  [
@@ -96,7 +97,7 @@ def test_format_group_report_check_group():
 
     report = report_formatter.format_group_report(groups)
 
-    assert report[1][0] == '1'
+    assert report[1][0] == xlsx.Cell('1')
 
 
 def test_format_individual_report_check_header_all_enabled():
@@ -110,7 +111,7 @@ def test_format_individual_report_check_header_all_enabled():
     data_config: models.Configuration = config.read_json(
         "./tests/test_files/configs/config_1.json")
     data_config["report_fields"] = report_config
-    report_formatter = reporter.ReportFormatter(data_config)
+    report_formatter = reporter.ReportFormatter(data_config, {})
 
     groups = [
         models.GroupRecord('1', [
@@ -133,16 +134,17 @@ def test_format_individual_report_check_header_all_enabled():
 
     report = report_formatter.format_individual_report(groups)
 
-    assert report[0] == ['Student Id', 'Filled out Survey', 'Disliked Students', 'Meets Dislike Requirement', 'Disliked students in group', 'Availability', 'Meets Availability Requirement', 'Availability Overlap', 'Preferred Students',
-                         'Meets Preferred Goal', 'Preferred students in group', 'Supplied Availability in Survey', 'Availability overlaps with others',
-                         'Group Id']
+    assert report[0] == [xlsx.Cell('Student Id'),  xlsx.Cell('Filled out Survey'), xlsx.Cell('Disliked Students'), xlsx.Cell('Meets Dislike Requirement'), xlsx.Cell('Disliked students in group'), xlsx.Cell('Availability'), xlsx.Cell('Meets Availability Requirement'), xlsx.Cell('Availability Overlap'), xlsx.Cell('Preferred Students'),
+                         xlsx.Cell('Meets Preferred Goal'), xlsx.Cell('Preferred students in group'), xlsx.Cell(
+                             'Supplied Availability in Survey'), xlsx.Cell('Availability overlaps with others'),
+                         xlsx.Cell('Group Id')]
 
 
 def test_format_config_flatten_headers():
     data_config: models.Configuration = config.read_json(
         "./tests/test_files/configs/config_1.json")
 
-    rpt = reporter.ReportFormatter(data_config)
+    rpt = reporter.ReportFormatter(data_config, {})
 
     # pylance ignore is needed here because we are testing a "private" method and the name gets mangled
     flattened_headers = rpt._ReportFormatter__config_report_headers()  # type: ignore
@@ -158,7 +160,7 @@ def test_format_config_flatten_data():
     data_config: models.Configuration = config.read_json(
         "./tests/test_files/configs/config_1.json")
 
-    rpt = reporter.ReportFormatter(data_config)
+    rpt = reporter.ReportFormatter(data_config, {})
 
     # pylance ignore is needed here because we are testing a "private" method and the name gets mangled
     flattened_data = rpt._ReportFormatter__get_flatten_config_data()  # type: ignore
