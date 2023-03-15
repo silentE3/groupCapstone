f71c4f7e298679e74e92775d601067731265d7a0
silentE3 <edwardchan2@comcast.net>
2023-03-15 14:56:15 -0700

    WIP Adding freeze panes to report

--- app/data/reporter.py
+++ app/data/reporter.py
@@ -28,6 +28,10 @@ def write_report(solutions: list[list[models.GroupRecord]], survey_data: models.
 
         xlsx_writer.write_sheet('individual_report_' +
                                 str(index + 1), formatted_data)
+        
+        worksheet = xlsx_writer.sheets.get('individual_report_' + str(index + 1), formatted_data)
+        worksheet.freeze_panes(0,1)
+
         xlsx_writer.write_sheet(
             'group_report_' + str(index + 1), group_formatted_report)
         xlsx_writer.write_sheet(
@@ -40,6 +44,8 @@ def write_report(solutions: list[list[models.GroupRecord]], survey_data: models.
     xlsx_writer.write_sheet(
         'survey_data', xlsx.convert_to_cells(survey_data.raw_rows))
 
+    
+    
     xlsx_writer.save()
 
 
