25fb08ff109221fcb9f0a82f5d1a37adb219ae42
zredinger-ccc <zach@cloud-team.com>
2023-03-15 22:52:30 -0500

    working version with threads

--- app/commands/group.py
+++ app/commands/group.py
@@ -2,8 +2,9 @@
 group contains all the commands for reading in the .csv data files and generating groups.
 Also includes reading in the configuration file.
 '''
-
+import concurrent.futures
 from pathlib import Path
+import threading
 import click
 from app import config, core, models
 from app.data import load, reporter
@@ -63,9 +64,8 @@ def group(surveyfile: str, configfile: str, reportfile: str, allstudentsfile: st
     ########## Run "first" grouping algorithm ##########
 
     # Run the grouping algorithm for all possible number of groups while keeping only the best solution found
-    best_solution_grouper_1: Grouper1 = __run_grouping_alg_1(
-        survey_data.records, config_data, min_max_num_groups[0], min_max_num_groups[1])
-
+    # best_solution_grouper_1: Grouper1 = __run_grouping_alg_1(
+    #     survey_data.records, config_data, min_max_num_groups[0], min_max_num_groups[1])
 
     ########## Run "second" grouping algorithm ##########
 
@@ -73,10 +73,8 @@ def group(surveyfile: str, configfile: str, reportfile: str, allstudentsfile: st
     best_solution_grouper_2: list[models.GroupRecord] = __run_grouping_alg_2(
         survey_data.records, config_data, min_max_num_groups[0], min_max_num_groups[1])
 
-
     ########## Output solutions report if configured ##########
-    solutions: list[list[models.GroupRecord]] = [
-        best_solution_grouper_1.best_solution_found, best_solution_grouper_2]
+    solutions: list[list[models.GroupRecord]] = [best_solution_grouper_2]
     click.echo(f'Writing report to: {report_filename}')
     reporter.write_report(
         solutions, survey_data, config_data, report_filename)
@@ -102,16 +100,41 @@ def __run_grouping_alg_2(records: list[models.SurveyRecord], config_data: models
 
     # Additional pre-processing: rank students based on their availability and num of people they are compatible with
     grouper_2.rank_students(records)
-
     best_solution_found: list[models.GroupRecord] = []
-    score: float = 0
-    for num_groups in range(min_num_groups, max_num_groups + 1):
-        grouper2 = grouper_2.Grouper2(records, config_data, num_groups)
-        group_result = grouper2.group_students()
-        if grouper2.grade_groups() > score or num_groups == min_num_groups:
-            score = grouper2.grade_groups()
-            best_solution_found = group_result
-    return best_solution_found
+    best_score: float = 0
+    cancel_event = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        exec_results = {executor.submit(run_grouper_2, records, config_data, num_groups, cancel_event):
+                        num_groups for num_groups in range(min_num_groups, max_num_groups + 1)}
+        _, not_done = concurrent.futures.wait(exec_results, timeout=0)
+        try:
+            while not_done:
+                _, not_done = concurrent.futures.wait(
+                    not_done, timeout=1)
+
+        except KeyboardInterrupt as exc:
+            click.echo('\nCancelling grouping...')
+            cancel_event.set()
+
+            raise exc
+        for future in concurrent.futures.as_completed(exec_results):
+            grouper = future.result()
+            score = grouper.grade_groups()
+            if score > best_score:
+                best_score = score
+                best_solution_found = grouper.groups
+
+        return best_solution_found
+
+
+def run_grouper_2(records, config_data, num_groups, cancel_event: threading.Event):
+    '''
+    runs grouper 2 with the given number of groups
+    '''
+    click.echo('running grouper 2 with ' + str(num_groups) + ' groups')
+    grouper2 = grouper_2.Grouper2(records, config_data, num_groups)
+    grouper2.group_students(cancel_event)
+    return grouper2
 
 
 def __report_filename(surveyfile: str, reportfile: str) -> str:
--- app/grouping/grouper_2.py
+++ app/grouping/grouper_2.py
@@ -3,6 +3,7 @@ module for an additional grouping algorithm implementation
 """
 import copy
 from random import randint
+import threading
 from typing import Optional
 from app import models
 from app.data import load
@@ -30,18 +31,20 @@ class Grouper2:
             self.groups.append(models.GroupRecord(f"group_{idx+1}"))
         self.console_printer: printer.GroupingConsolePrinter = printer.GroupingConsolePrinter()
 
-    def group_students(self) -> list[models.GroupRecord]:
+    def group_students(self, cancel_event : threading.Event) -> list[models.GroupRecord]:
         """
         initiates the grouping process
         """
         self.prepare_students_for_grouping()
         while len(self.students) > 0:
+            if cancel_event.is_set():
+                return self.groups
             student = self.students.pop()
             self.add_student_to_group(student)
         self.console_printer.print('balancing groups that need more members')
-        self.balance_groups()
+        self.balance_groups(cancel_event)
         self.console_printer.print('optimizing groups for best solution')
-        self.optimize_groups()
+        self.optimize_groups(cancel_event)
         # self.console_printer.print(f'group size={self.group_count}, score={self.grade_groups()}')
 
         # Clear any final print statement in preparation for it to be overwritten
@@ -104,11 +107,13 @@ class Grouper2:
                     break
             member_count += 1
 
-    def balance_groups(self):
+    def balance_groups(self, cancel_event : threading.Event):
         '''
         balances groups
         '''
         for group in self.groups:
+            if cancel_event.is_set():
+                return
             if len(group.members) < self.target_group_size - self.target_group_margin_lower:
                 self.console_printer.print(
                     f'balancing group: {group.group_id} with size {len(group.members)}')
@@ -169,7 +174,7 @@ class Grouper2:
                     group.members.append(member)
                     break
 
-    def optimize_groups(self):
+    def optimize_groups(self, cancel_event: threading.Event):
         '''
         creates grouping optimizations by swapping users
         if 5 operations result in the same score, it will end with the assumption that it has done the best it will do
@@ -181,6 +186,8 @@ class Grouper2:
         dup_score_count = 0
         # loop through the specified # of grouping passes
         for group_pass in range(self.grouping_passes):
+            if cancel_event.is_set():
+                return
             # track previous scores
             if prev_score == self.grade_groups():
                 dup_score_count += 1
