c501e7a815a8908d088f66f05b6f7660a9dfe4ac
Armand <aaaltive@asu.edu>
2023-03-28 20:06:24 -0700

    reorganize the locations of the quality tests that were created only to adhere to quality plan

--- tests/test_group.py
+++ tests/test_group.py
@@ -3,8 +3,7 @@ from click.testing import CliRunner
 import math
 from app.commands import group
 from tests.test_utils.helper_functions import verify_groups
-from tests.test_utils.helper_functions import verify_all_students
-from os.path import exists
+
 
 runner = CliRunner()
 
@@ -84,27 +83,6 @@ def test_group_3():
     assert "Error:" not in response.output
 
     os.remove('./tests/test_files/survey_results/test_3_report.xlsx')
-
-def test_group_quality_2():
-    '''
-    Test of grouping 16 students with a target group size of 3 (does not divide evenly,
-    but still possible to maintain +1, [group sizes of 3 or 4]).
-    '''
-
-    response = runner.invoke(group.group, [
-                             './tests/test_files/survey_results/Example_Survey_Results_16.csv', '--configfile', './tests/test_files/configs/config_16.json', '--reportfile', './tests/test_files/survey_results/test_16_report.xlsx'])
-    assert response.exit_code == 0
-
-    expected_students = ['adumble4', 'triddle8', 'dmalfoy7',
-                         'rweasle3', 'hgrange2', 'rhagrid5', 'hpotter1', 'nlongbo6',
-                         'adumble4_2', 'triddle8_2', 'dmalfoy7_2',
-                         'rweasle3_2', 'hgrange2_2', 'rhagrid5_2', 'hpotter1_2', 'nlongbo6_2']
-    verify_groups('./tests/test_files/survey_results/test_16_report.xlsx', 4,
-                  5, expected_students)
-    # Verify "Error:" is NOT included in the output
-    assert "Error:" not in response.output
-
-    os.remove('./tests/test_files/survey_results/test_16_report.xlsx')
 
 def test_group_size_not_possible():
     '''
@@ -197,22 +175,3 @@ def test_alt_command_args_1():
     assert response.output.endswith(
         'Writing report to: test_verify_and_report_file_name_1_report.xlsx\n')
     os.remove('test_verify_and_report_file_name_1_report.xlsx')
-
-
-def test_complete_solution():
-    '''
-    This test is to verify that when a surveyfile containing 100 students is provided to the program, with a target
-    group size of 5 with a tolarance of +1, the program will produce a grouping solution with all 100 students grouped
-    '''
-    complete_solution = runner.invoke(group.group, ['./tests/test_files/survey_results/Example_Survey_Results_100.csv',
-                                                   '-c', './tests/test_files/configs/config_100.json'])
-    expected_min_num_groups = 17
-    expected_max_num_groups = 20
-    expected_students = [(lambda x: f"asurite{x}")(x) for x in range(1, 101)]
-
-    assert complete_solution.exit_code == 0
-
-    verify_all_students('./tests/test_files/survey_results/Example_Survey_Results_100_report.xlsx', expected_min_num_groups,
-                  expected_max_num_groups, expected_students)
-
-    os.remove('./tests/test_files/survey_results/Example_Survey_Results_100_report.xlsx')
